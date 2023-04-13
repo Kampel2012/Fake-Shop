377ccd47fa4dc9db6689ccc557189b85adc5cf48
Anton Glazunov <kampel2012@yandex.ru>
2023-04-13 19:13:26 +0500

    fix цвета при наведении

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Category = (props) => {
   return (
-    <button className="px-2 py-1 border border-gray-950 text-lg font-semibold">
+    <button className="px-2 py-1 border border-gray-950 text-lg font-semibold hover:text-orange-400 hover:border-orange-400">
       {props.name}
     </button>
   );
